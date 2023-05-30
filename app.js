9842c9e29336f6bd01dc772a38b543dafb806aa0
leticiaschmitt <leticiaschmitt304@gmail.com>
2023-05-30 15:56:14 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ console.log("Rodando o sistema de gerenciamento de jogo")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("A Letícia fez esse commit sem voce ver na aula!")
+console.log("mais um log de teste")
 
 
