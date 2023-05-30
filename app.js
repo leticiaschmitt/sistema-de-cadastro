fd700baea01ae208e9c23e8ba41ca194fe427a61
leticiaschmitt <leticiaschmitt304@gmail.com>
2023-05-30 09:10:19 -0300

    voltando para o estado da Leticia

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogo")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("A Letícia fez esse commit sem voce ver na aula!")
-console.log("Enviando para o GitHUb")
 
 
